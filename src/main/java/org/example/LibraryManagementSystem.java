0bf33feba62cba51c750000a4ec632f0bb4f40d1
Harsh Mehta <mehtaharsh733@gmail.com>
2024-08-31 16:35:31 +0530

    Test Case Failed: Cannot find addBook Mehtod.

--- src/main/java/org/example/LibraryManagementSystem.java
+++ src/main/java/org/example/LibraryManagementSystem.java
@@ -1,6 +1,15 @@
 package org.example;
 
+import org.example.model.Book;
+
+import java.util.*;
+
+
 public class LibraryManagementSystem {
+    private static final List<Book> availableBooks = new ArrayList<>();
+    public static List<Book> getAvailableBooks() {
+        return Collections.unmodifiableList(availableBooks);
+    }
 
 
 
--- src/test/java/org/example/LibraryManagementSystemTest.java
+++ src/test/java/org/example/LibraryManagementSystemTest.java
@@ -4,7 +4,13 @@ import org.example.model.Book;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class LibraryManagementSystemTest {
+    public static final List<Book> availableBooks = LibraryManagementSystem.getAvailableBooks();
 
     LibraryManagementSystem lms;
 
@@ -14,7 +20,12 @@ class LibraryManagementSystemTest {
         lms = new LibraryManagementSystem();
     }
     @Test
-    public void addBookTest(){
-        Book book=new Book("title","987-123-456-6789","Harsh",2003);
+    public void addBookTest() {
+        Book book = new Book("title", "987-123-123-9876", "author", 2004);
+        // number of books before adding
+        int noOfBooks = availableBooks.size();
+        lms.addBook(book);
+        assertEquals(noOfBooks + 1, availableBooks.size());
+        assertTrue(availableBooks.contains(book));
     }
 }
